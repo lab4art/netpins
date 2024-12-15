e81db81af14a5d53ed40cdadc959be5746b9f8d6
Matej Lazar <matejonnet@gmail.com>
2024-12-15 01:20:16 +0100

    Optional static IP config.

--- src/config.h
+++ src/config.h
@@ -4,3 +4,8 @@
 
 #define WIFI_SSID ""
 #define WIFI_PASS ""
+
+#define STATIC_IP      IPAddress(0, 0, 0, 0) // set non zero to enable static IP
+#define GATEWAY        IPAddress(0, 0, 0, 0)
+#define SUBNET         IPAddress(0, 0, 0, 0)
+#define DNS            IPAddress(0, 0, 0, 0)
